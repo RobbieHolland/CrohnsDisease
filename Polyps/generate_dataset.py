d89218995691c1bebb52ecbcc4e620b27a7913e6
Robert <Robert@Roberts-MBP.broadband>
2019-08-08 15:13:00 +0100

    [RH] Remove irrelevant/deprecated files.

--- Polyps/generate_dataset.py
+++
@@ -1,8 +0,0 @@
-'''
-Generates CSV of slices
-'''
-
-from data.handling.generate_tfrecord import TFRecordGenerator
-
-generator = TFRecordGenerator('./data/cases/', '/vol/bitbucket/rh2515/CT_Colonography', 'data/tfrecords')
-# generator.generate_train_test(0.2, 'new')
--- run_polyps.sh
+++
@@ -1,7 +0,0 @@
-python3 run.py \
-Polyps_CT \
-Polyps/data/tfrecords/train_volume_balanced.tfrecords \
-Polyps/data/tfrecords/test_volume_balanced.tfrecords \
-5,256,256 \
--bS=10 \
--lD=logdir_polyps
